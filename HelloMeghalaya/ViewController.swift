032d3c1c61b7afd6c9224ae0813028362404e6f0
padmaja <padmaja@saranyu.in>
2026-08-12 15:28:55 +0530

    Initial Commit

---
+++ HelloMeghalaya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloMeghalaya
+//
+//  Created by SaranyuMac1 on 12/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
